328a10fc218af7e28636433f60a7bf14f2c8027a
kevbell90 <93627264+kevbell90@users.noreply.github.com>
2021-11-02 20:36:50 -0400

    Add files via upload

---
+++ RDP User Creation.ps1
@@ -0,0 +1,11 @@
+﻿# My simple RDP user creation script
+Write-Host "Enter a username:"
+$NameForUser = Read-Host
+Write-Host "Enter a password"
+$SecurePassword = Read-Host -AsSecureString
+
+# Create a new user
+New-ADUser -Name $NameForUser -AccountPassword $SecurePassword -ChangePasswordAtLogon $false -PassThru | Enable-ADAccount
+
+# Add new user to RDP group
+Add-ADGroupMember -Identity "RDP Users" -Members $NameForUser
